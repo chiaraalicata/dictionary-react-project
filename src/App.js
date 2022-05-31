4d337f66de4542172a03cd6da02c3d5b3e811667
chiaraalicata <74967879+chiaraalicata@users.noreply.github.com>
2022-05-31 13:21:36 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import dictionary from "./dictionary.jpeg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={dictionary} className="App-logo img-fluid" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
